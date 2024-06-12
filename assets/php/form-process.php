46c56a41578513c9ef27b3bf61314ebc8652d9cf
sufiyan-2141 <saheaven298@gmail.com>
2024-06-13 00:12:53 +0530

    Add files via upload

---
+++ assets/php/form-process.php
@@ -0,0 +1,78 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MSG SUBJECT
+if (empty($_POST["msg_subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $msg_subject = $_POST["msg_subject"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+// phone
+if (empty($_POST["phone"])) {
+    $errorMSG .= "Mobile number is required ";
+} else {
+    $phone = $_POST["phone"];
+}
+
+
+$EmailTo = "info@webschedio.com";
+
+$Subject = "Enquiry from website contact form";
+
+// prepare email body text
+$Body = "This email is from website contact form<br>";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $msg_subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
